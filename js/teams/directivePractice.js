39f4cb81606e1e4be43e04a6bce10e54680455de
KatyaEames <katya.eames@gmail.com>
2015-01-28 11:29:05 -0700

    js

---
+++ js/teams/directivePractice.js
@@ -0,0 +1,10 @@
+var deleter = function(arr) {
+	var track = {};
+	for(var i = 0; i < arr.length; i++) {
+		if(track[arr[i]]) {
+			arr.splice(i, 1);
+		} else {
+			track[arr[i]] = true;
+		}
+	}
+}
